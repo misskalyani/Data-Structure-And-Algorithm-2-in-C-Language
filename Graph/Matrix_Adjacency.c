42c8437f492ad12cc881622a97ba3b9f49a7ec26
kalyani chaudhari <138003080+misskalyani@users.noreply.github.com>
2024-02-11 16:54:13 +0530

    Add files via upload

---
+++ Graph/Matrix_Adjacency.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int main()
+{
+	int a[20][20],i,n,j,sum_indegree,sum_outdegree,total;
+	printf("Enter Limit : ");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		for(j=1;j<=n;j++)
+		{
+		
+			
+				printf("Enter Edge Betwwen v%d v%d : ",i,j);
+				scanf("%d",&a[i][j]);
+			
+		}
+	}
+	printf("\nAjacency Matrix::\n ");
+	for(i=1;i<=n;i++)
+	{
+		for(j=1;j<=n;j++)
+		{
+			printf(" \t%d",a[i][j]);
+		}
+		printf("\n");
+	}
+	printf("\nVertex\tindegree\toutdegree\ttotal\n");
+	for(i=1;i<=n;i++)
+	{
+		sum_indegree=sum_outdegree=0;
+		for(j=1;j<=n;j++)
+		{
+			sum_indegree=sum_indegree+a[j][i];
+			sum_outdegree=sum_outdegree+a[i][j];
+		}
+		printf("\nv%d\t    \t%d\t    \t%d\t    %d",i,sum_indegree,sum_outdegree,sum_indegree+sum_outdegree);
+	}
+}
